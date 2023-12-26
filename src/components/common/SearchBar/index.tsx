a727b8ef12febfa608797d834abc25b7ff6d3793
Yelynn Oh <yoyelynn@gmail.com>
2023-12-27 01:27:54 +0900

    #24 feat: useInput 훅 활용 및 코드 리뷰 반영

--- src/components/common/SearchBar/index.tsx
+++ src/components/common/SearchBar/index.tsx
@@ -1,51 +1,37 @@
 'use client';
 
-import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
+import { InputHTMLAttributes } from 'react';
 
 import SearchIcon from '/public/SearchIcon.svg';
 import DeleteIcon from '/public/DeleteIcon.svg';
 
 import Input from '../Input';
 
+import useInput from '@hooks/useInput';
+
 interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
 }
 
 export default function SearchBar({ icon }: SearchBarProps) {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-
-  const clearInput = () => {
-    setInputValue('');
-  };
-
-  let iconElement = null;
-  if (inputValue) {
-    iconElement = (
-      <div onClick={clearInput} className="cursor-pointer">
-        <DeleteIcon />
-      </div>
-    );
-  } else {
-    iconElement = (
-      <div>
-        <SearchIcon />
-      </div>
-    );
-  }
+  const [text, onTextChange, resetText] = useInput('');
 
   return (
     <div>
       <div className="flex items-center px-4 py-2">
         <div className="w-[40px] h-[40px]">{icon}</div>
         <Input
-          rightItem={iconElement}
-          onChange={handleChange}
-          onClick={clearInput}
-          value={inputValue}
+          rightItem={
+            text ? (
+              <button onClick={resetText}>
+                <DeleteIcon />
+              </button>
+            ) : (
+              <SearchIcon />
+            )
+          }
+          value={text}
+          onChange={onTextChange}
         />
       </div>
     </div>
