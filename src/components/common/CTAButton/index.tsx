f52961428da06c27d9e025a1b0413d744b1b43f1
y00eunji <aine0227@naver.com>
2023-12-26 19:09:58 +0900

    #23 feat: 로그인 버튼 svg 추가 및 버튼 컴포넌트 수정

--- src/components/common/CTAButton/index.tsx
+++ src/components/common/CTAButton/index.tsx
@@ -13,7 +13,7 @@ export default function CTAButton({
       {...props}
       type={type}
       className={cn(
-        'w-full p-[16px] h-[56px] rounded-[24px] text-center text-white body-14-bold bg-primary-500 active:bg-primary-300 disabled:bg-gray-300',
+        'w-full p-[16px] h-[56px] rounded-[24px] flex flex-row items-center justify-center gap-[8px] text-white body-14-bold bg-primary-500 active:bg-primary-300 disabled:bg-gray-300',
         className,
       )}
     >
