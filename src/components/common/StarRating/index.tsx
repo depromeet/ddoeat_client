f180ac76acb8d2cc17a64659cde5729184d33012
Yelynn Oh <yoyelynn@gmail.com>
2023-12-27 15:45:49 +0900

    #25 chore: 별 svg 파일명 통일

--- src/components/common/StarRating/index.tsx
+++ src/components/common/StarRating/index.tsx
@@ -1,17 +1,18 @@
-import FilledStarSmall from 'public/assets/img/FilledStarSmall.svg';
-import EmptyStarSmall from 'public/assets/img/EmptyStarSmall.svg';
+import FilledStarSmall from 'public/assets/img/filled_star_24.svg';
+import DefaultStarSmall from 'public/assets/img/default_star_24.svg';
 
 interface StarRatingProps {
   rating: number;
 }
 
 export default function StarRating({ rating }: StarRatingProps) {
-  // 별점 불러올 때 소수점 반올림할 지 버릴지 논의 필요
+  // TODO: 별점 불러올 때 소수점 반올림할 지 버릴지 논의 필요
   const filledStars = Math.round(rating); // Filled Star 개수 결정
 
+  // TODO: 별점 구간별로 별 ui 어떻게 보여줄 지 디자인과 논의 후 수정 필요
   const getStarsNum = Array.from({ length: 5 }).map((_, index) => (
     <div key={index}>
-      {index < filledStars ? <FilledStarSmall /> : <EmptyStarSmall />}
+      {index < filledStars ? <FilledStarSmall /> : <DefaultStarSmall />}
     </div>
   ));
 
