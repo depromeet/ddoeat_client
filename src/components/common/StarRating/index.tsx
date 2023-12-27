a9a99a3d21e326ea8a66163aee48a3ed2a2b8190
Yelynn Oh <yoyelynn@gmail.com>
2023-12-27 14:28:29 +0900

    #25 feat: StarRating 컴포넌트 로직 수정

--- src/components/common/StarRating/index.tsx
+++ src/components/common/StarRating/index.tsx
@@ -6,25 +6,21 @@ interface StarRatingProps {
 }
 
 export default function StarRating({ rating }: StarRatingProps) {
-  const filledStars = Math.floor(rating); // 소수점 이하를 버리고, Filled Star 개수 결정
+  const filledStars = Math.round(rating); // Filled Star 개수 결정
   const emptyStars = 5 - filledStars; // 전체 별의 개수에서 Filled Star 개수를 뺀 나머지는 Empty Star 개수
 
-  const getStarNum = (count: number, type: 'filled' | 'empty') => {
-    const stars = [];
-    for (let i = 0; i < count; i++) {
-      stars.push(
-        <div key={i}>
-          {type === 'filled' ? <FilledStarSmall /> : <EmptyStarSmall />}
-        </div>,
-      );
-    }
-    return stars;
-  };
+  const getStarNum = (count: number, type: 'filled' | 'empty') =>
+    Array.from({ length: count }).map((_, index) => (
+      <div key={index}>
+        {type === 'filled' ? <FilledStarSmall /> : <EmptyStarSmall />}
+      </div>
+    ));
 
   return (
     <div className="flex flex-row items-center">
-      {getStarNum(filledStars, 'filled')}
-      {getStarNum(emptyStars, 'empty')}
+      {getStarNum(filledStars, 'filled').concat(
+        getStarNum(emptyStars, 'empty'),
+      )}
     </div>
   );
 }
