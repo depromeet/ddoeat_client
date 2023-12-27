9f0558db5a74e1c5a4ff454daff65258e868cff8
YOOJS1205 <junsang8921@gmail.com>
2023-12-27 18:21:51 +0900

    visitNumFlag 분기처리, 맛집 컴포넌트 스타일 수정

--- src/components/Search/Restaurant.tsx
+++ src/components/Search/Restaurant.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import VisitNumberFlag from '@components/common/VisitNumberFlag';
 import More from 'public/assets/icon/More.svg';
 import restaurantImg from 'public/assets/img/search/restaurant.png';
+import cn from '@utils/cn';
 
 interface RestaurantProps extends HTMLAttributes<HTMLLIElement> {
   restaurantImgUrl?: string;
@@ -12,6 +13,7 @@ interface RestaurantProps extends HTMLAttributes<HTMLLIElement> {
   location: string;
   visitNum: number;
   hasMoreOption: boolean;
+  isLast: boolean;
 }
 
 export default function Restaurant({
@@ -21,10 +23,15 @@ export default function Restaurant({
   location,
   visitNum,
   hasMoreOption,
+  isLast,
 }: RestaurantProps) {
   return (
     <li className="w-full px-4 pt-2 flex justify-between items-center">
-      <div className="flex justify-between items-center pt-2 pb-4 w-full border-b-gray-100 border-b-[1px]">
+      <div
+        className={`${cn('flex justify-between items-center pt-2 pb-4 w-full', {
+          'border-b-gray-100 border-b-[1px]': isLast,
+        })}`}
+      >
         <div className="flex gap-2">
           <Image
             src={restaurantImgUrl ?? restaurantImg}
@@ -35,7 +42,7 @@ export default function Restaurant({
           <div className="flex flex-col gap-2">
             <div className="flex gap-1">
               <p className="body-16-bold">{restaurantName}</p>
-              <VisitNumberFlag visitNum={visitNum} />
+              {visitNum && <VisitNumberFlag visitNum={visitNum} />}
             </div>
             <div className="flex caption-12-bold">
               <p className="after:inline-block after:w-[0.5px] after:h-[10px] after:bg-gray-300 after:mx-2">
--- src/components/common/VisitNumberFlag.tsx
+++ src/components/common/VisitNumberFlag.tsx
@@ -5,9 +5,7 @@ interface VisitNumberFlagProps {
 export default function VisitNumberFlag({ visitNum }: VisitNumberFlagProps) {
   return (
     <div className="rounded-[32px] px-2 py-1 bg-primary-100">
-      <p className="caption-10-bold text-primary-500">
-        {visitNum ?? '-'}번 방문
-      </p>
+      <p className="caption-10-bold text-primary-500">{visitNum}번 방문</p>
     </div>
   );
 }
