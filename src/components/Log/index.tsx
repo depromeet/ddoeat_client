f724dd3840a35a9db045adbbe318707b4f5b50f6
Yelynn Oh <yoyelynn@gmail.com>
2023-12-28 21:02:54 +0900

    #25 style : StarRating에 별점 추가 및 별점 정수여도 소수점으로 보이게 수정

--- src/components/Log/index.tsx
+++ src/components/Log/index.tsx
@@ -35,9 +35,6 @@ export default function MyLog({
         <p className="text-gray-700 body-14-bold">{date}</p>
       </div>
       <div className="flex flex-row items-center w-full h-full gap-x-[8px]">
-        {/* <svg className="w-[16px] h-full stroke-primary-300">
-          <line x1="50%" y1="0" x2="50%" y2="100%" strokeWidth="1" />
-        </svg> */}
         <div className="flex flex-col w-full h-full gap-y-[8px] pl-[16px] border-l-[2px] border-primary-300">
           <div className="flex flex-row justify-between items-center w-full h-[126px] px-[16px] py-[12px] gap-x-[8px] rounded-[24px] bg-gray-50 border border-gray-100">
             <div className="flex flex-col">
@@ -48,8 +45,7 @@ export default function MyLog({
                 {/* <VisitNumFlag visitNum={visitNum} /> */}
                 <MenuTypeFlag menuType={menuType} />
               </div>
-              <div className="flex flex-row items-center gap-x-[4px]">
-                <span className="body-16-bold text-primary-500">{rating}</span>
+              <div className="flex items-center gap-x-[4px]">
                 <StarRating rating={rating} />
               </div>
             </div>
--- src/components/common/StarRating/index.tsx
+++ src/components/common/StarRating/index.tsx
@@ -9,6 +9,8 @@ export default function StarRating({ rating }: StarRatingProps) {
   // TODO: 별점 불러올 때 소수점 반올림할 지 버릴지 논의 필요
   const filledStars = Math.round(rating); // Filled Star 개수 결정
 
+  const formattedRating = rating.toFixed(1);
+
   // TODO: 별점 구간별로 별 ui 어떻게 보여줄 지 디자인과 논의 후 수정 필요
   const getStarsNum = Array.from({ length: 5 }).map((_, index) => (
     <div key={index}>
@@ -16,5 +18,10 @@ export default function StarRating({ rating }: StarRatingProps) {
     </div>
   ));
 
-  return <div className="flex flex-row items-center">{getStarsNum}</div>;
+  return (
+    <div className="flex flex-row gap-x-[4px] items-center body-16-bold text-primary-500">
+      <span> {formattedRating} </span>
+      <div className="flex flex-row"> {getStarsNum}</div>
+    </div>
+  );
 }
