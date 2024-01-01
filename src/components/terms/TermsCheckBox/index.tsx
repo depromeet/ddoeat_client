99d3fb394c6dc48cdcf3cb6afbb7d8310e9aa29f
YOOJS1205 <junsang8921@gmail.com>
2024-01-01 23:43:22 +0900

    header, NavigationButton, TermsButton 컴포넌트 작성

---
+++ src/components/terms/TermsCheckBox/index.tsx
@@ -0,0 +1,23 @@
+import { HTMLAttributes } from 'react';
+
+import CheckBoxFill from 'public/assets/icon24/checkbox_fill_24.svg';
+import CheckboxDefault from 'public/assets/icon24/checkbox_default_24.svg';
+
+interface TermsCheckBoxProps extends HTMLAttributes<HTMLButtonElement> {
+  isCheckAllRequiredTerms: boolean;
+}
+
+export default function TermsCheckBox({
+  isCheckAllRequiredTerms,
+  onClick,
+}: TermsCheckBoxProps) {
+  return (
+    <button
+      className="flex gap-[4px] items-center w-full h-[56px] bg-gray-100 p-[16px] rounded-[32px]"
+      onClick={onClick}
+    >
+      {isCheckAllRequiredTerms ? <CheckBoxFill /> : <CheckboxDefault />}
+      <p className="text-gray-900 body-16-bold">약관 전체 동의</p>
+    </button>
+  );
+}
