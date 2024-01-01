3161139369e827f7be8ae0e0030aa1e7286df62a
YOOJS1205 <junsang8921@gmail.com>
2024-01-01 17:42:38 +0900

    리뷰 페이지에서 BottomNaviagtion 삭제

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -4,7 +4,6 @@ import { useState, useCallback } from 'react';
 
 import ImageUploader from '@components/common/ImageUploader';
 import StarRating from '@components/review/StarRating';
-import BottomNavigation from '@components/common/BottomNavigation';
 
 export default function Page() {
   const [rating, setRating] = useState(0);
@@ -16,7 +15,6 @@ export default function Page() {
     <div>
       <StarRating rating={rating} onClick={handleRating} />
       <ImageUploader />
-      <BottomNavigation />
     </div>
   );
 }
