5dcf12075ed3fb32144e79df88904166a5fc72cd
y00eunji <aine0227@naver.com>
2024-01-03 12:32:00 +0900

    #66 feat: SearchField 컴포넌트 작성

---
+++ src/components/common/SearchField/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import CTAButton from '../CTAButton';
+
+import SearchIcon from 'public/assets/icon24/search_24.svg';
+
+export default function SearchField() {
+  const router = useRouter();
+
+  const handleSearchButtonClick = () => {
+    // TODO: 검색 페이지로 이동 시키기
+    router.push('/search');
+  };
+
+  return (
+    <CTAButton
+      onClick={handleSearchButtonClick}
+      className="justify-between text-gray-500 body-14-regular bg-white active:bg-white"
+    >
+      <p>맛집을 검색하세요</p>
+      <SearchIcon />
+    </CTAButton>
+  );
+}
