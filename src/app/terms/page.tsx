44f1f6c725dafb43915bdcb30eb7356a9a44a3ce
YOOJS1205 <junsang8921@gmail.com>
2024-01-04 01:19:35 +0900

    약관 상태 관련 로직 리팩토링

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -6,41 +6,35 @@ import { useRouter } from 'next/navigation';
 import TermsCheckBoxButton from '@components/terms/TermsCheckBoxButton';
 import NavigationButton from '@components/terms/NavigationButton';
 import TermsItem from '@components/terms/TermsItem';
-
-type TermsItem = 'terms1' | 'terms2' | 'terms3';
+import { TERMS } from 'src/constants/terms';
 
 export default function Page() {
   const { push } = useRouter();
 
-  const [termsChecked, setTermsChecked] = useState({
-    terms1: false,
-    terms2: false,
-    terms3: false,
-  });
+  const [termsState, setTermsState] = useState(
+    TERMS.map((term) => ({ ...term, isChecked: false })),
+  );
+
+  // NOTE: 모든 약관이 클릭 되었는가, 필수 약관이 클릭 되었는가
+  const isTermsAllChecked = termsState.every((term) => term.isChecked);
+  const isRequiredTermsAllChecked = termsState.every(
+    (term) => !term.required || term.isChecked,
+  );
 
   // NOTE: 전체 약관 클릭 여부 변경 함수
-  const handleClickTermsCheckBoxButton = () => {
-    if (termsChecked.terms1 && termsChecked.terms2 && termsChecked.terms3) {
-      setTermsChecked({
-        terms1: false,
-        terms2: false,
-        terms3: false,
-      });
-    } else {
-      setTermsChecked({
-        terms1: true,
-        terms2: true,
-        terms3: true,
-      });
-    }
+  const handleCheckAll = () => {
+    setTermsState((prevTerm) =>
+      prevTerm.map((term) => ({ ...term, isChecked: !isTermsAllChecked })),
+    );
   };
 
   // NOTE: 개별 약관 클릭 여부 변경 함수
-  const handleClickTermsItem = (termItem: TermsItem) => {
-    setTermsChecked((prev) => ({
-      ...prev,
-      [termItem]: !prev[termItem],
-    }));
+  const handleClickTermsItem = (id: string) => () => {
+    setTermsState((prevTerm) =>
+      prevTerm.map((term) =>
+        term.id === id ? { ...term, isChecked: !term.isChecked } : term,
+      ),
+    );
   };
 
   const handleClickConfirmButton = () => {
@@ -55,31 +49,22 @@ export default function Page() {
       </h1>
       <div className="px-[16px] py-[12px]">
         <TermsCheckBoxButton
-          isCheckAllRequiredTerms={
-            termsChecked.terms1 && termsChecked.terms2 && termsChecked.terms3
-          }
-          onClick={handleClickTermsCheckBoxButton}
+          isCheckAllRequiredTerms={isTermsAllChecked}
+          onClick={handleCheckAll}
         />
       </div>
       <ul>
-        <TermsItem
-          title="또잇또잇 이용약관 (필수)"
-          isChecked={termsChecked.terms1}
-          onChangeCheckState={() => handleClickTermsItem('terms1')}
-        />
-        <TermsItem
-          title="위치정보 이용약관 동의 (필수)"
-          isChecked={termsChecked.terms2}
-          onChangeCheckState={() => handleClickTermsItem('terms2')}
-        />
-        <TermsItem
-          title="제 3자 정보제공 동의"
-          isChecked={termsChecked.terms3}
-          onChangeCheckState={() => handleClickTermsItem('terms3')}
-        />
+        {TERMS.map((term, index) => (
+          <TermsItem
+            key={term.id}
+            title={term.title}
+            isChecked={termsState[index].isChecked}
+            onChangeCheckState={handleClickTermsItem(term.id)}
+          />
+        ))}
       </ul>
       <NavigationButton
-        disabled={!termsChecked.terms1 || !termsChecked.terms2}
+        disabled={!isRequiredTermsAllChecked}
         className="absolute bottom-0 left-0"
         onClick={handleClickConfirmButton}
       >
