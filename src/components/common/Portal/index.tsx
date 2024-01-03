2790f5cccf42dce2b4e6fd88a17b042988d2301d
sjoleee <cent7425@gmail.com>
2024-01-04 01:09:41 +0900

    feat: Portal, AnimatePortal 작성

---
+++ src/components/common/Portal/index.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { PropsWithChildren, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+export default function Portal({ children }: PropsWithChildren) {
+  const [container, setContainer] = useState<Element | null>(null);
+
+  useEffect(() => {
+    if (document) {
+      setContainer(document.body);
+    }
+  }, []);
+
+  if (!container) return null;
+
+  return createPortal(children, container);
+}
