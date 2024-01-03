7e6829c7e2f61fef6e8dcd3566b9ce1cbc4b67cd
y00eunji <aine0227@naver.com>
2024-01-03 10:51:09 +0900

    #51 chore: svg이름 변경

--- src/components/Home/FloatingButton/index.tsx
+++ src/components/Home/FloatingButton/index.tsx
@@ -3,8 +3,8 @@
 import { ButtonHTMLAttributes } from 'react';
 
 import CTAButton from '@components/common/CTAButton';
-import InActiveLocation from 'public/assets/icon24/loction_default_24.svg';
-import ActiveLocation from 'public/assets/icon24/location_filled_24.svg';
+import InActiveLocationIcon from 'public/assets/icon24/loction_default_24.svg';
+import ActiveLocationIcon from 'public/assets/icon24/location_filled_24.svg';
 
 // NOTE: isActive는 예시입니다. 부모 컴포넌트에서 클릭하는 위치가 어딘지에 따라 상태가 달라집니다.
 interface FloatingButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -20,7 +20,7 @@ export default function FloatingButton({
       {...props}
       className="w-[50px] h-[50px] rounded-[16px] p-0 bg-white shadow-[0_4px_15.4px_0_rgba(0,0,0,0.25)] active:bg-white"
     >
-      {isActive ? <ActiveLocation /> : <InActiveLocation />}
+      {isActive ? <ActiveLocationIcon /> : <InActiveLocationIcon />}
     </CTAButton>
   );
 }
