a7c49154caa323cbcb496c66a43c394973ec0a2d
y00eunji <aine0227@naver.com>
2024-01-06 15:16:03 +0900

    #67 feat: Report 컴포넌트 className 추가 및 타입 수정

--- src/components/StoreDetail/Report/index.tsx
+++ src/components/StoreDetail/Report/index.tsx
@@ -1,12 +1,18 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes } from 'react';
 
-interface ReportProps {
-  children: ReactNode;
-}
+import cn from '@utils/cn';
 
-export default function Report({ children }: ReportProps) {
+export default function Report({
+  className,
+  children,
+}: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="w-[167.5px] h-[130px] bg-primary-300 rounded-[24px] text-white flex flex-col justify-end items-center ">
+    <div
+      className={cn(
+        'w-[168px] h-[130px] bg-primary-300 rounded-[24px] text-white flex flex-col justify-end items-center ',
+        className,
+      )}
+    >
       {children}
     </div>
   );
