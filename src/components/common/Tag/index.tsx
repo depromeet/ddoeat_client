8beea80e6e059b8070aec8a49f55cc20656eaef9
Yelynn Oh <yoyelynn@gmail.com>
2024-01-06 17:21:37 +0900

    #68 chore: 코드 리뷰 반영 및 style 수정

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -1,45 +1,24 @@
-import { ComponentProps, ReactNode } from 'react';
+import { HTMLAttributes } from 'react';
 
 import cn from '@utils/cn';
 
 type TagSizeType = 'small' | 'large';
-interface TagProps extends ComponentProps<'div'> {
-  children: ReactNode;
+interface TagProps extends HTMLAttributes<HTMLDivElement> {
   size: TagSizeType;
 }
-interface TagStylesAttributes {
-  height: string;
-  width: string;
-  px: string;
-  textFont: string;
-}
 
-const getTagAttributes = (type: TagSizeType): TagStylesAttributes => {
-  switch (type) {
-    case 'small':
-      return {
-        height: 'h-[24px]',
-        width: 'w-[43px]',
-        px: 'px-[8px]',
-        textFont: 'caption-10-bold',
-      };
-    case 'large':
-      return {
-        height: 'h-[32px]',
-        width: 'w-[53px]',
-        px: 'px-[12px]',
-        textFont: 'caption-12-bold',
-      };
-  }
+const tagStyleAttributes = {
+  small: 'h-[24px] w-[43px] px-[8px] caption-10-bold',
+  large: 'h-[32px] px-[12px] caption-12-bold',
 };
 
 export default function Tag({ children, className, size }: TagProps) {
-  const { height, width, px, textFont } = getTagAttributes(size);
-  console.log(height, width, px, textFont);
+  const tagStyles = tagStyleAttributes[size];
+
   return (
     <div
       className={cn(
-        `flex justify-items items-center justify-between gap-[4px] rounded-[32px] py-[4px] ${px} ${height} ${width} ${textFont}`,
+        `inline-flex justify-items items-center justify-between gap-[4px] rounded-[32px] py-[4px] ${tagStyles}`,
         className,
       )}
     >
