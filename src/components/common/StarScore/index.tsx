865fd566b482b651681b5541eaa893f234db5677
Yelynn Oh <yoyelynn@gmail.com>
2024-01-09 01:30:38 +0900

    #71 feat: 코드 리뷰 반영해 StarScore 수정

--- src/components/common/StarScore/index.tsx
+++ src/components/common/StarScore/index.tsx
@@ -1,32 +1,29 @@
-import FilledStar50Icon from '/public/assets/icon24/star50_24.svg';
-import DefaultStarSmallIcon from '/public/assets/icon24/star_default_24.svg';
-import FilledStarSmallIcon from '/public/assets/icon24/star_filled_24.svg';
+import StarByRating from './StarByRating';
+
+const TOTAL_STAR_COUNT = 5;
+
+const renderStars = (formattedRating: number) => {
+  const filledStars = Math.floor(formattedRating);
+  const decimalRating = formattedRating - filledStars;
+
+  return Array.from({ length: TOTAL_STAR_COUNT }).map((_, index) => {
+    const isFilledStar = index < filledStars;
+    const isHalfStar = index === filledStars && decimalRating >= 0.5;
+    return <StarByRating key={index} filled={isFilledStar} half={isHalfStar} />;
+  });
+};
 
 interface StarScoreProps {
   rating: number;
 }
 
 export default function StarScore({ rating }: StarScoreProps) {
-  const formattedRating = rating.toFixed(1);
-  const filledStars = Math.floor(rating);
-  const decimalRating = rating - Math.floor(rating);
+  const formattedRating = Number(rating.toFixed(1));
 
-  const renderStars = Array.from({ length: 5 }).map((_, index) => {
-    const StarIcon =
-      index < filledStars ? (
-        <FilledStarSmallIcon />
-      ) : index === filledStars && decimalRating >= 0.5 ? (
-        <FilledStar50Icon />
-      ) : (
-        <DefaultStarSmallIcon />
-      );
-
-    return <div key={index}>{StarIcon}</div>;
-  });
   return (
-    <div className="flex flex-row gap-x-[4px] items-center body-16-bold text-primary-500">
-      <span> {formattedRating} </span>
-      <div className="flex flex-row"> {renderStars}</div>
+    <div className="flex gap-[4px] items-center body-16-bold text-primary-500">
+      <span>{formattedRating}</span>
+      <div className="flex">{renderStars(formattedRating)}</div>
     </div>
   );
 }
