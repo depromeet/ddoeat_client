6d89dedfcf14279d068275425fb2052841a99f39
Yelynn Oh <yoyelynn@gmail.com>
2024-01-09 01:02:44 +0900

    #91 feat: 온보딩 layout 설정

---
+++ src/app/onboarding/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+interface OnboardingLayout {
+  children: ReactNode;
+}
+
+export default function layout({ children }: OnboardingLayout) {
+  return <div className="h-screen w-screen bg-gray-100">{children}</div>;
+}
