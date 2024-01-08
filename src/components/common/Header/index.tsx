d15f3265bd72e94d78bf5d9b24e50f4bb029cd8e
y00eunji <aine0227@naver.com>
2024-01-08 23:39:13 +0900

    #93 chore: 헤더 컴포넌트 className 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,13 +2,15 @@
 
 import { useRouter } from 'next/navigation';
 
+import cn from '@utils/cn';
 import LeftArrow from 'public/assets/icon24/left_arrow_24.svg';
 
 interface HeaderProps {
   children?: React.ReactNode;
+  className?: string;
 }
 
-export default function Header({ children }: HeaderProps) {
+export default function Header({ children, className }: HeaderProps) {
   const { back } = useRouter();
 
   const handleClickBackButton = () => {
@@ -18,7 +20,7 @@ export default function Header({ children }: HeaderProps) {
   return (
     <div className="flex items-center px-[16px] py-[8px] w-full h-[56px]">
       <button className="w-[40px] h-[40px]" onClick={handleClickBackButton}>
-        <LeftArrow />
+        <LeftArrow className={cn('fill-gray-700', className)} />
       </button>
       {/* NOTE: 저희 서비스에서는 텍스트 혹은 search input이 들어갑니다. Header 컴포넌트 내부에 넣어주시면 됩니다. */}
       {children}
