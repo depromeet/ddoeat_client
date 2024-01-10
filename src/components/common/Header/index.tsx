e373319a33bfebcc527e84f9c265a002f271d80b
y00eunji <aine0227@naver.com>
2024-01-10 18:06:47 +0900

    #93 feat: Header 컴포넌트 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -8,14 +8,9 @@ import LeftArrow from 'public/assets/icon24/left_arrow_24.svg';
 interface HeaderProps {
   children?: React.ReactNode;
   className?: string;
-  arrowColor?: string;
 }
 
-export default function Header({
-  children,
-  className,
-  arrowColor,
-}: HeaderProps) {
+export default function Header({ children, className }: HeaderProps) {
   const { back } = useRouter();
 
   const handleClickBackButton = () => {
@@ -30,7 +25,7 @@ export default function Header({
       )}
     >
       <button className="w-[40px] h-[40px]" onClick={handleClickBackButton}>
-        <LeftArrow className={cn('fill-gray-700', arrowColor)} />
+        <LeftArrow />
       </button>
       {/* NOTE: 저희 서비스에서는 텍스트 혹은 search input이 들어갑니다. Header 컴포넌트 내부에 넣어주시면 됩니다. */}
       {children}
