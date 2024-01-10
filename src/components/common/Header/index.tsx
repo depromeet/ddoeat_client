98521ac937a3cd52dc4446b3db7d485039d3d5c7
y00eunji <aine0227@naver.com>
2024-01-10 17:41:43 +0900

    #93 feat: Header 컴포넌트 className, arrowColor 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -8,9 +8,14 @@ import LeftArrow from 'public/assets/icon24/left_arrow_24.svg';
 interface HeaderProps {
   children?: React.ReactNode;
   className?: string;
+  arrowColor?: string;
 }
 
-export default function Header({ children, className }: HeaderProps) {
+export default function Header({
+  children,
+  className,
+  arrowColor,
+}: HeaderProps) {
   const { back } = useRouter();
 
   const handleClickBackButton = () => {
@@ -18,9 +23,14 @@ export default function Header({ children, className }: HeaderProps) {
   };
 
   return (
-    <div className="flex items-center px-[16px] py-[8px] w-full h-[56px]">
+    <div
+      className={cn(
+        'flex items-center px-[16px] py-[8px] w-full h-[56px]',
+        className,
+      )}
+    >
       <button className="w-[40px] h-[40px]" onClick={handleClickBackButton}>
-        <LeftArrow className={cn('fill-gray-700', className)} />
+        <LeftArrow className={cn('fill-gray-700', arrowColor)} />
       </button>
       {/* NOTE: 저희 서비스에서는 텍스트 혹은 search input이 들어갑니다. Header 컴포넌트 내부에 넣어주시면 됩니다. */}
       {children}
