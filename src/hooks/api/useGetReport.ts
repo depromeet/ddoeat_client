4d38d37312d444cbb21ff204b136f0b8831f8f2b
y00eunji <aine0227@naver.com>
2024-01-15 18:54:48 +0900

    #87 feat: useGetReport 훅 작성

---
+++ src/hooks/api/useGetReport.ts
@@ -0,0 +1,24 @@
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+
+import { axiosRequest } from '@api/api-config';
+
+interface ReportProps {
+  storeId: string;
+  storeMainImageUrl: string;
+  mostVisitedCount: number;
+  totalRevisitedCount: number;
+}
+
+const getReport = (storeId: string): Promise<ReportProps> => {
+  return axiosRequest('get', `/api/v1/stores/${storeId}/reports`);
+};
+
+export const useGetReport = (
+  storeId: string,
+): UseQueryResult<ReportProps, Promise<ReportProps>> => {
+  return useQuery({
+    queryKey: ['get-report', storeId],
+    queryFn: () => getReport(storeId),
+    enabled: !!storeId,
+  });
+};
