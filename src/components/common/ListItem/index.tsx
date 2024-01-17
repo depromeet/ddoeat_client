b6c705cb39ec86aa792c3092fae9499822e7957e
y00eunji <aine0227@naver.com>
2024-01-18 00:43:08 +0900

    #88 feat: ListItem & Log에 삭제 모달 적용

--- src/components/common/ListItem/index.tsx
+++ src/components/common/ListItem/index.tsx
@@ -1,4 +1,6 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
+
+import DeleteModal from '../DeleteModal';
 
 import cn from '@utils/cn';
 import TrashIcon from 'public/assets/icon24/trash_24.svg';
@@ -18,24 +20,46 @@ export default function ListItem({
 }: PropsWithChildren<BasicListItem>) {
   const { mutate: deleteLog } = useDeleteLog();
 
+  const [isModalShowing, setModalShowing] = useState(false);
+
   const handleClickDeleteButton = () => {
+    setModalShowing(true);
+  };
+
+  const handleDeleteConfirm = () => {
     // TODO: 추후 로그 삭제 로직 확정
     deleteLog(listId);
+    setModalShowing(false);
   };
+
+  const handleCancel = () => {
+    setModalShowing(false);
+  };
+
   return (
-    <li className="w-full pl-[24px] pr-[16px] flex justify-between items-center">
-      <div
-        className={`${cn('flex justify-between items-center pt-2 pb-4 w-full', {
-          'border-b-gray-100 border-b-[1px]': !isLast,
-        })}`}
-      >
-        <div className="flex flex-col gap-[9px]">{children}</div>
-        {hasDeleteOption && (
-          <button onClick={handleClickDeleteButton}>
-            <TrashIcon />
-          </button>
-        )}
-      </div>
-    </li>
+    <>
+      <li className="w-full pl-[24px] pr-[16px] flex justify-between items-center">
+        <div
+          className={`${cn(
+            'flex justify-between items-center pt-2 pb-4 w-full',
+            {
+              'border-b-gray-100 border-b-[1px]': !isLast,
+            },
+          )}`}
+        >
+          <div className="flex flex-col gap-[9px]">{children}</div>
+          {hasDeleteOption && (
+            <button onClick={handleClickDeleteButton}>
+              <TrashIcon />
+            </button>
+          )}
+        </div>
+      </li>
+      <DeleteModal
+        isShowing={isModalShowing}
+        onClick={handleDeleteConfirm}
+        onCancel={handleCancel}
+      />
+    </>
   );
 }
--- src/components/mypage/Log/index.tsx
+++ src/components/mypage/Log/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useState } from 'react';
 
 import StarRating from '@components/common/StarScore';
 import Tag from '@components/common/Tag';
@@ -10,6 +10,7 @@ import DotIcon from '/public/assets/icon20/dot_20.svg';
 
 import TrashIcon from 'public/assets/icon24/trash_24.svg';
 import { useDeleteLog } from '@hooks/api/useDeleteLog';
+import DeleteModal from '@components/common/DeleteModal';
 
 interface MyLogProps extends HTMLAttributes<HTMLLIElement> {
   date: string;
@@ -33,55 +34,77 @@ export default function MyLog({
   log,
 }: MyLogProps) {
   const { mutate: deleteLog } = useDeleteLog();
+  const [isModalShowing, setModalShowing] = useState(false);
 
   const handleClickDeleteButton = () => {
+    setModalShowing(true);
+  };
+
+  const handleDeleteConfirm = () => {
     // TODO: 추후 로그 삭제 로직 확정
     deleteLog(logId);
+    setModalShowing(false);
+  };
+
+  const handleCancel = () => {
+    setModalShowing(false);
   };
 
   return (
-    <li className="flex flex-col w-full h-full bg-white p-[16px] gap-y-[8px]">
-      <div className="flex items-center">
-        <div className="flex w-full h-[20px] gap-x-[8px] items-center">
-          <DotIcon />
-          <p className="text-gray-700 body-14-bold">{date}</p>
-        </div>
-        <button onClick={handleClickDeleteButton}>
-          <TrashIcon />
-        </button>
-      </div>
-      <div className="flex items-center w-full h-full gap-x-[8px] justify-end">
-        <div className="flex flex-col w-[calc(100%-8px)] h-full gap-y-[8px] pl-[16px] border-l-[1px] border-primary-300">
-          <div className="flex justify-between items-center w-full h-[126px] px-[16px] py-[12px] gap-x-[8px] rounded-[24px] bg-gray-50 border border-gray-100">
-            <div className="flex flex-col">
-              <span className="mb-[8px] w-full h-full body-16-bold text-gray-900">
-                {storeName}
-              </span>
-              <div className="flex items-center mb-[4px] gap-x-[4px]">
-                <Tag size={'small'} className="text-primary-500 bg-primary-100">
-                  내 방문 {myVisitNum}번
-                </Tag>
-                <Tag size={'small'} className="text-gray-500 bg-gray-50">
-                  {menuType}
-                </Tag>
-              </div>
-              <div className="flex items-center gap-x-[4px]">
-                <StarRating rating={rating} />
-              </div>
-            </div>
-            {storeImgUrl && (
-              <Image
-                src={storeImgUrl}
-                alt={storeName}
-                width={100}
-                height={100}
-                objectFit="cover"
-              />
-            )}
+    <>
+      <li className="flex flex-col w-full h-full bg-white p-[16px] gap-y-[8px]">
+        <div className="flex items-center">
+          <div className="flex w-full h-[20px] gap-x-[8px] items-center">
+            <DotIcon />
+            <p className="text-gray-700 body-14-bold">{date}</p>
           </div>
-          <span className="body-14-regular text-gray-700 break-all">{log}</span>
+          <button onClick={handleClickDeleteButton}>
+            <TrashIcon />
+          </button>
         </div>
-      </div>
-    </li>
+        <div className="flex items-center w-full h-full gap-x-[8px] justify-end">
+          <div className="flex flex-col w-[calc(100%-8px)] h-full gap-y-[8px] pl-[16px] border-l-[1px] border-primary-300">
+            <div className="flex justify-between items-center w-full h-[126px] px-[16px] py-[12px] gap-x-[8px] rounded-[24px] bg-gray-50 border border-gray-100">
+              <div className="flex flex-col">
+                <span className="mb-[8px] w-full h-full body-16-bold text-gray-900">
+                  {storeName}
+                </span>
+                <div className="flex items-center mb-[4px] gap-x-[4px]">
+                  <Tag
+                    size={'small'}
+                    className="text-primary-500 bg-primary-100"
+                  >
+                    내 방문 {myVisitNum}번
+                  </Tag>
+                  <Tag size={'small'} className="text-gray-500 bg-gray-50">
+                    {menuType}
+                  </Tag>
+                </div>
+                <div className="flex items-center gap-x-[4px]">
+                  <StarRating rating={rating} />
+                </div>
+              </div>
+              {storeImgUrl && (
+                <Image
+                  src={storeImgUrl}
+                  alt={storeName}
+                  width={100}
+                  height={100}
+                  objectFit="cover"
+                />
+              )}
+            </div>
+            <span className="body-14-regular text-gray-700 break-all">
+              {log}
+            </span>
+          </div>
+        </div>
+      </li>
+      <DeleteModal
+        isShowing={isModalShowing}
+        onClick={handleDeleteConfirm}
+        onCancel={handleCancel}
+      />
+    </>
   );
 }
