c2ebafb9ad7e227ea79285c7276a79b0d68e36a1
y00eunji <aine0227@naver.com>
2024-01-25 19:28:52 +0900

    #88 chore : handleClickSettingButton 핸들러 이름 변경

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -11,12 +11,12 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   const router = useRouter();
 
-  const handleSettingButtonClick = () => router.push('/setting');
+  const handleClickSettingButton = () => router.push('/setting');
 
   return (
     <main className='bg-[url("/assets/image/mypage/background.png")] bg-cover'>
       <Header className=" [&>*>*]:fill-white justify-between">
-        <SettingIcon onClick={handleSettingButtonClick} />
+        <SettingIcon onClick={handleClickSettingButton} />
       </Header>
       {children}
     </main>
