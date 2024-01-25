55397e0500b99fb2c1f03c0a2e73f8534f12935d
YOOJS1205 <junsang8921@gmail.com>
2024-01-26 00:33:00 +0900

    앱 화면에서 넘어왔을 때의 분기 처리 로직 작성

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,28 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import Cookies from 'js-cookie';
 
 export function middleware(request: NextRequest) {
+  // NOTE: splash 화면으로 넘어왔을 때 쿠키 내의 토큰 여부에 따른 리다이렉트 로직
+  const isFromApp = request.nextUrl.searchParams.get('fromApp');
+
+  if (isFromApp === 'true') {
+    const accessToken = request.cookies.get('accessToken');
+    const refreshToken = request.cookies.get('refreshToken');
+
+    const url = request.nextUrl.clone();
+
+    if (accessToken && refreshToken) {
+      url.pathname = '/';
+    } else {
+      url.pathname = '/login';
+    }
+
+    const response = NextResponse.redirect(url);
+
+    return response;
+  }
+
   const accessToken = request.nextUrl.searchParams.get('accessToken');
   const refreshToken = request.nextUrl.searchParams.get('refreshToken');
   const isFirstLogin = request.nextUrl.searchParams.get('isFirst') as string;
