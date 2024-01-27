e79f013e67e606e4f02cb99e47a7696651277a2f
YOOJS1205 <junsang8921@gmail.com>
2024-01-27 14:40:09 +0900

    주석 삭제

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -21,7 +21,6 @@ export interface ApiResponse<T> {
   };
 }
 
-// 테스트를 위해 jsonplaceholder url를 넣었습니다.
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
