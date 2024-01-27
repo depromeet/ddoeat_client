eea361d1039f7599955bfff635414c5f1a5cc64b
y00eunji <aine0227@naver.com>
2024-01-27 12:16:24 +0900

    #88 style : DeleteModal 스타일 변경

--- src/components/common/DeleteModal/index.tsx
+++ src/components/common/DeleteModal/index.tsx
@@ -28,9 +28,9 @@ export default function DeleteModal({
         />
         <div className="w-screen h-screen flex items-center justify-center">
           <div className="w-[300px] h-[188px] bg-white flex items-center justify-center flex-col p-[24px] rounded-[24px] z-toast">
-            <div className="mb-[32px]">
-              <p className="body-16-bold mb-[12px]"> 정말 기록을 삭제할까요?</p>
-              <p className="caption-12-regular text-center">
+            <div className="mb-[32px]  text-center">
+              <p className="body-16-bold mb-[12px]">정말 기록을 삭제할까요?</p>
+              <p className="caption-12-regular">
                 기록 삭제시 복구가 불가능하고, <br /> 등록된 가게 정보가 사라질
                 수 있어요.
               </p>
