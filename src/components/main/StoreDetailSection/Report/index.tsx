3ca4dee3dd19bdd01e3762b2d1ba6ce7f12c3169
y00eunji <aine0227@naver.com>
2024-01-28 23:14:35 +0900

    #138 style: 리포트 스타일 수정

--- src/components/main/StoreDetailSection/Report/index.tsx
+++ src/components/main/StoreDetailSection/Report/index.tsx
@@ -6,7 +6,7 @@ export default function Report() {
   return (
     <div className="px-[16px]  pb-[8px]">
       <SectionTitle>또잇또잇 리포트</SectionTitle>
-      <div className="flex gap-[8px] mx-[16px] my-[4px]">
+      <div className="flex gap-[8px] mx-[16px] my-[4px]  justify-center items-center">
         <TopVisit />
         <ReVisit />
       </div>
