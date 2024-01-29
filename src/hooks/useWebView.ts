81352f6ef22d13a8e942703d8d6c49c23eb1678b
Yelynn Oh <yoyelynn@gmail.com>
2024-01-30 01:21:54 +0900

    #159 chore: 코드리뷰 반영해 useWebView 수정

--- src/hooks/useWebView.ts
+++ src/hooks/useWebView.ts
@@ -4,9 +4,11 @@ import Cookies from 'js-cookie';
 const useWebView = () => {
   useEffect(() => {
     if (!window?.ReactNativeWebView) return;
-    else {
-      const accessToken = Cookies.get('accessToken');
-      const refreshToken = Cookies.get('refreshToken');
+
+    const accessToken = Cookies.get('accessToken');
+    const refreshToken = Cookies.get('refreshToken');
+
+    if (accessToken != null && refreshToken != null) {
       const message = JSON.stringify({ accessToken, refreshToken });
       window.ReactNativeWebView.postMessage(message);
     }
