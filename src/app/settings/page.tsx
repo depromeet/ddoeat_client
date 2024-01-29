79ddcfb0701c2b3b73da84d54d1349be539c8fe7
YOOJS1205 <junsang8921@gmail.com>
2024-01-30 01:05:30 +0900

    모달 컴포넌트 작성

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -6,10 +6,14 @@ import { SETTINGS, Setting } from '@constants/settings';
 import RightArrowIcon from 'public/assets/icon24/right_arrow_24.svg';
 import ArrowSquareIcon from 'public/assets/icon16/arrow_square_16.svg';
 import { useLogout } from '@hooks/api/useLogout';
+import Modal from '@components/common/Modal';
+import useControlModal from '@utils/useControlModal';
 
 export default function Page() {
   const { push } = useRouter();
   const { mutate: logout } = useLogout();
+  const { isModalShowing, handleOpenModal, handleCloseModal } =
+    useControlModal();
 
   const handleClickSettingItem = (setting: Setting) => () => {
     if (setting.url) {
@@ -17,25 +21,47 @@ export default function Page() {
     }
 
     if (setting.title === '로그아웃') {
-      logout();
+      handleOpenModal();
     }
   };
 
+  const handleClickLogoutButton = () => {
+    logout();
+  };
+
   return (
-    <ul className="pt-[56px]">
-      {SETTINGS.map((setting, index) => (
-        <li
-          key={index}
-          className="flex justify-between items-center py-[8px] pl-[24px] pr-[16px] h-[56px] cursor-pointer border-b-[1px] border-gray-100"
-          onClick={handleClickSettingItem(setting)}
-        >
-          <div className="flex gap-[4px] items-center">
-            <p className="body-16-bold">{setting.title}</p>
-            {setting.url && <ArrowSquareIcon />}
-          </div>
-          {setting.url && <RightArrowIcon />}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="pt-[56px]">
+        {SETTINGS.map((setting, index) => (
+          <li
+            key={index}
+            className="flex justify-between items-center py-[8px] pl-[24px] pr-[16px] h-[56px] cursor-pointer border-b-[1px] border-gray-100"
+            onClick={handleClickSettingItem(setting)}
+          >
+            <div className="flex gap-[4px] items-center">
+              <p className="body-16-bold">{setting.title}</p>
+              {setting.url && <ArrowSquareIcon />}
+            </div>
+            {setting.url && <RightArrowIcon />}
+          </li>
+        ))}
+      </ul>
+      <Modal
+        isShowing={isModalShowing}
+        text="로그아웃"
+        subText="로그아웃하시겠습니까?"
+        controls={[
+          {
+            buttonText: '취소',
+            buttonHandler: handleCloseModal,
+          },
+          {
+            buttonText: '삭제',
+            buttonHandler: handleClickLogoutButton,
+          },
+        ]}
+        onCancel={handleCloseModal}
+      />
+    </>
   );
 }
