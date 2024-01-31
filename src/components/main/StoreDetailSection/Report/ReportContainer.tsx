d3e5f860c0502093c619c60d4346415041e1361f
y00eunji <aine0227@naver.com>
2024-02-01 06:36:10 +0900

    #222 fix : 또잇 리포트 넓이 수정

--- src/components/main/StoreDetailSection/Report/ReportContainer.tsx
+++ src/components/main/StoreDetailSection/Report/ReportContainer.tsx
@@ -9,7 +9,7 @@ export default function ReportContainer({
   return (
     <div
       className={cn(
-        'w-[168px] h-[130px] bg-primary-300 rounded-[24px] text-white flex flex-col justify-end items-center ',
+        'w-1/2 h-[130px] bg-primary-300 rounded-[24px] text-white flex flex-col justify-end items-center ',
         className,
       )}
     >
--- src/components/main/StoreDetailSection/Report/index.tsx
+++ src/components/main/StoreDetailSection/Report/index.tsx
@@ -4,9 +4,9 @@ import TopVisit from './TopVisit';
 
 export default function Report() {
   return (
-    <div className="px-[16px]  pb-[8px]">
+    <div className="px-[16px] pb-[8px]">
       <SectionTitle>또잇또잇 리포트</SectionTitle>
-      <div className="flex gap-[8px] mx-[16px] my-[4px]  justify-center items-center">
+      <div className="flex gap-[8px] justify-center items-center">
         <TopVisit />
         <ReVisit />
       </div>
