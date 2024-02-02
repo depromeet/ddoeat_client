bdb872e75675b33f5af3b2e80a5aaf3da509d311
YOOJS1205 <junsang8921@gmail.com>
2024-02-02 19:05:07 +0900

    애플 로그인 주석처리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,12 @@ import { useRouter } from 'next/navigation';
 
 import CTAButton from '@components/common/CTAButton';
 import DdoeatLogo from 'public/assets/ddoeat_logo.svg';
-import AppleLogo from 'public/assets/icon24/apple_logo_24.svg';
+// import AppleLogo from 'public/assets/icon24/apple_logo_24.svg';
 import KakaoLogo from 'public/assets/icon24/kakao_logo_24.svg';
 
 export default function Page() {
   const { push } = useRouter();
+
   const handleClickKakaoLoginButton = () => {
     // TODO: 추후에 env=development, env=production으로 변경 요청
     const env = process.env.NODE_ENV === 'development' ? 'local' : 'dev';
@@ -17,9 +18,9 @@ export default function Page() {
     );
   };
 
-  const handleClickAppleLoginButton = () => {
-    // TODO: 애플 로그인 구현
-  };
+  // const handleClickAppleLoginButton = () => {
+  //   // TODO: 애플 로그인 구현
+  // };
 
   return (
     <div className="absolute right-0 left-0 top-0 bottom-0">
@@ -27,21 +28,20 @@ export default function Page() {
         <DdoeatLogo />
         <div className="flex flex-col gap-[16px] w-full px-[24px] py-[16px]">
           <CTAButton
-            // NOTE: body-16-bold 스타일 미적용 확인
             className="body-16-bold bg-system-kakaoYellow text-gray-900"
             onClick={handleClickKakaoLoginButton}
           >
             <KakaoLogo />
             카카오로 시작하기
           </CTAButton>
-          <CTAButton
+          {/* <CTAButton
             // NOTE: body-16-bold 스타일 미적용 확인
             className="body-16-bold border-gray-900 border-[1px] bg-white text-gray-900"
             onClick={handleClickAppleLoginButton}
           >
             <AppleLogo />
             Apple로 시작하기
-          </CTAButton>
+          </CTAButton> */}
         </div>
       </div>
     </div>
