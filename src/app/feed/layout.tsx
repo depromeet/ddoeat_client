bbfb33400a211eb69773a2f83d7bd53522ad4470
YOOJS1205 <junsang8921@gmail.com>
2024-04-10 17:44:04 +0900

    피드 레이아웃 컴포넌트 분기 처리, 피드 작성 완료 라우팅 수정

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -1,14 +1,21 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const pathname = usePathname();
   return (
     <div>
-      <h1 className="flex top-0 items-center justify-center w-full h-[56px] body-16-bold border-b-[1px] border-gray-100">
-        피드
-      </h1>
-      <div className="top-[56px]">{children}</div>
+      {pathname === '/feed' && (
+        <h1 className="flex top-0 items-center justify-center w-full h-[56px] body-16-bold border-b-[1px] border-gray-100">
+          피드
+        </h1>
+      )}
+      <div>{children}</div>
     </div>
   );
 }
