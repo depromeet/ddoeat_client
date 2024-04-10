ee75cbb83c795b46d12300437740cd860a1ad250
Yelynn Oh <yoyelynn@gmail.com>
2024-04-10 22:28:11 +0900

    #321 feat: type 쿼리로 initialTab 결정하는 거 추가

--- src/app/follow/[id]/page.tsx
+++ src/app/follow/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 import FollowerList from '@components/following/FollowerList';
 import Tab from '@components/mypage/Tab';
@@ -9,13 +9,16 @@ import { useGetFollowingList } from '@hooks/api/useGetFollowingList';
 export default function Page() {
   const pathname = usePathname();
   const userId = Number(pathname.split('/').pop());
+  const searchParams = useSearchParams();
+
+  const type = searchParams.get('type');
 
   const { data: followerList } = useGetFollowingList(userId, 'FOLLOWER');
   const { data: followingList } = useGetFollowingList(userId, 'FOLLOWER');
 
   return (
     <div>
-      <Tab.Group initialTab="follower">
+      <Tab.Group initialTab={type ? type : 'follower'}>
         <div className="h-[76px] w-full flex sticky top-[40px] bg-white z-above rounded-t-[24px]">
           <Tab.Header>
             <Tab.Item value="follower" className="w-[1/2] my-[10px]">
